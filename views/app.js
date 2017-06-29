5ec6b1f951cab7ab053a03e1812d7a8b011ada57
gauravparashar29 <gauravparashar294@gmail.com>
2017-06-29 14:42:38 +0530

    Commit 2.0.0.2.z

--- views/app.js
+++ views/app.js
@@ -2077,7 +2077,7 @@ var exambazaar = angular.module('exambazaar', ['ui.router', 'ngMaterial', 'ngAri
         
         
         $scope.years = ["2017","2016","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003"];
-        $scope.reviewTags = ["Great Faculty", "Supportive Administration", "Value for Money", "Hi-Tech", "Exhaustive Content", "Realistic Test Series"];
+        $scope.reviewTags = ["Great Faculty", "Supportive Administration", "Value for Money", "Tech Powered", "Exhaustive Content", "Effective Test Series"];
         $scope.addRemoveReviewTag = function(reviewTag){
             if(!$scope.userReview.tags){
                 $scope.userReview.tags = [];
