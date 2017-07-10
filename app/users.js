72ca0a395b1ad962d1081a84e342a1bd6f7856c0
gauravparashar29 <gauravparashar294@gmail.com>
2017-07-10 13:22:23 +0530

    Commit 2.0.0.3.a.11

--- app/users.js
+++ app/users.js
@@ -658,7 +658,7 @@ router.post('/fbSave', function(req, res) {
 
 //to get all users
 router.get('/', function(req, res) {
-    user.find({}, {userType: 1, basic: 1, mobile: 1, facebook: 1, email: 1, image: 1, fbemail: 1, fbimage: 1}, function(err, docs) {
+    user.find({_created: {  $gte : "2017-07-09T00:00:00.000Z"}}, {userType: 1, basic: 1, mobile: 1, facebook: 1, email: 1, image: 1, fbemail: 1, fbimage: 1, _created: 1}, function(err, docs) {
     if (!err){
         res.json(docs);
     } else {throw err;}
