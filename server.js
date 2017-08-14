09eeebb57e5833621854894c8b5251c9845b2d36
Gaurav Parashar <gaurav@exambazaar.com>
2017-08-14 18:05:24 +0530

    Commit 2.0.0.3.e

--- server.js
+++ server.js
@@ -78,14 +78,14 @@ app.get('/*', function (req, res, next) {
 
 
 
-app.get('*', function(req, res, next) {
+/*app.get('*', function(req, res, next) {
     if (req.get('x-forwarded-proto') != "https") {
         res.set('x-forwarded-proto', 'https');
         res.redirect('https://' + req.get('host') + req.url);
     } else {
         next();     
     }
-});
+});*/
 
 
 
