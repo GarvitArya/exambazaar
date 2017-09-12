57e12907a869ad8819ee97f118e3df4a587b5ee1
Gaurav Parashar <gaurav@exambazaar.com>
2017-09-12 18:41:41 +0530

    Commit 2.0.0.3.h.15

--- views/app.js
+++ views/app.js
@@ -8181,7 +8181,7 @@ var exambazaar = angular.module('exambazaar', ['ui.router', 'ngMaterial', 'ngAri
         }
         
         $scope.showLevel = 0;
-        var allowedCities = ['New Delhi', 'Bangalore', 'Kanpur', 'Allahabad', 'Bhopal', 'Varanasi', 'Dehradun', 'Raipur', 'Noida', 'Ghaziabad', 'Dhanbad', 'Bhubaneshwar', 'Jammu', 'Amritsar', 'Gwalior', 'Indore', 'Ranchi', 'Mysore','Pondicherry', 'Kochi', 'Gurgaon', 'Bathinda'];
+        var allowedCities = ['New Delhi', 'Bangalore', 'Kanpur', 'Allahabad', 'Bhopal', 'Varanasi', 'Dehradun', 'Raipur', 'Noida', 'Ghaziabad', 'Dhanbad', 'Bhubaneshwar', 'Jammu', 'Amritsar', 'Gwalior', 'Indore', 'Ranchi', 'Mysore','Pondicherry', 'Kochi', 'Gurgaon', 'Bathinda', 'Jalandhar'];
         
         if($cookies.getObject('sessionuser')){
             
