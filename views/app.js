da1bb029c7b7adf58d0b0157c2ef46344df7132f
Gaurav Parashar <gaurav@exambazaar.com>
2017-10-24 11:52:42 +0530

    Commit 2.0.0.3.l.10

--- views/app.js
+++ views/app.js
@@ -20805,11 +20805,11 @@ function getLatLng(thisData) {
             url: '/',
             views: {
                 'header':{
-                    templateUrl: 'header.html',
+                    /*templateUrl: 'header.html',*/
                     
                 },
                 'body':{
-                    templateUrl: 'p0.html',
+                    templateUrl: 'maintenance.html',
                     controller: 'p0Controller'
                 },
                 'footer': {
