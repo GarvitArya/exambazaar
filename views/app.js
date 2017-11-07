4ae4f4e5c36bee078abd31193c238575875c35dc
Gaurav Parashar <gaurav@exambazaar.com>
2017-11-08 00:18:41 +0530

    Commit 2.0.0.3.n.15

--- views/app.js
+++ views/app.js
@@ -34959,6 +34959,26 @@ exambazaar.directive('onlyDigits', function () {
       }
     };
 });
+
+exambazaar.directive('onlyLetters', function () {
+    return {
+    require: 'ngModel',
+    link: function(scope, element, attr, ngModelCtrl) {
+      function fromUser(text) {
+        var transformedInput = text.replace(/[^a-zA-Z]/g, '');
+        //console.log(transformedInput);
+        if (transformedInput !== text) {
+          ngModelCtrl.$setViewValue(transformedInput);
+          ngModelCtrl.$render();
+        }
+        return transformedInput;
+      }
+      ngModelCtrl.$parsers.push(fromUser);
+    }
+  };
+});
+
+
 exambazaar.directive("limitTo", [function() {
     return {
         restrict: "A",
