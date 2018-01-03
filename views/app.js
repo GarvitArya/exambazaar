df2a330799ab380d923e34b3e091e17076baaa28
gauravparashar29 <gauravparashar294@gmail.com>
2018-01-03 19:17:03 +0530

    Commit 2.0.0.3.u.14

--- views/app.js
+++ views/app.js
@@ -32112,7 +32112,7 @@ function getLatLng(thisData) {
                 if(subquestion.type != 'numerical'){
                     subquestion.type = 'numerical';
                 }
-                subquestion.type.numericalAnswerType = 'Range';
+                subquestion.numericalAnswerType = 'Range';
                 if(!subquestion.numericalAnswers){
                     subquestion.numericalAnswers = [''];
                 }
